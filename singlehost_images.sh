001212d16003c8339e8536412d3e5af06e8254f7
Sebastien Bonami <sebastien.bonami.1@ens.etsmtl.ca>
2014-11-30 02:28:15 -0500

    Small correction

--- singlehost_images.sh
+++ singlehost_images.sh
@@ -3,6 +3,6 @@
 #Pulling and building Docker images
 docker pull google/cadvisor
 docker pull ubuntu:14.04
-docker build --rm=true -t snap Dockerfiles/snap-adam-avocado-spark
+docker build --rm=true -t snap-adam-avocado-spark Dockerfiles/snap-adam-avocado-spark
 docker build --rm=true -t oraclejdk_7 Dockerfiles/oraclejdk_7
 docker build --rm=true -t spark_1.1.0-prebuilthadoop2.3 Dockerfiles/spark_1.1.0-prebuilthadoop2.3
